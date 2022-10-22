bf310f5e076310d6b4e021a576016b094e24bf27
Joseph Neill <josephneill2@gmail.com>
2022-10-22 09:33:18 -0400

    timeline and bar graph together

--- manuscripts.js
+++ manuscripts.js
@@ -7,11 +7,15 @@ window.manuscripts = [
         summary: 'The books of the New Testament were written by various authors sometime between approximately 50AD and 100AD. (141 papyri, 322-41=281 uncials, 2926? (2911? or 3011?) minuscules, 2484 lectionaries)',
         date: 70, /* 50-100 */
         qty: 5386, /* 5856 */
-        range: [117, 1565], /* [125, ...] */
-        papyri: {
-            count: 141,
-            range: [],
+        quantity: {
+            fragments: 141,
+            manuscripts: 5715,
         },
+        earliest: {
+            fragment: 125,
+            manuscript: 350,
+        },
+        range: [125, 1565], /* [117, ...] */
         /* 72, 48, 47, 24 
         2177, 615, 721, 329 */
         /* minuscules biblical content = 2070 */
@@ -21,21 +25,45 @@ window.manuscripts = [
         summary: 'This data is just an approximate.',
         date: -730, /* -700 */
         qty: 1900,
+        quantity: {
+            fragments: null,
+            manuscripts: 1900,
+        },
+        earliest: {
+            fragment: null,
+            manuscript: 300,
+        },
         range: [300,1200] /* [-415, ...] */
     },
     {
-        name: 'Annals',
+        name: 'Annals (Tacitus)',
         summary: 'Still need to do better research on this...',
         date: 100,
         qty: 2,
+        quantity: {
+            fragments: 0,
+            manuscripts: 2,
+        },
+        earliest: {
+            fragment: null,
+            manuscript: 850,
+        },
         range: [850, 1040]
     },
     {
-        name: 'Herodotus (Histories)',
+        name: 'Histories (Herodotus)',
         summary: 'Histories by Herodotus was written between 480BC and 425BC. Extant manuscripts include 9 principal manuscripts and around <a target="_blank" href="https://papyri.info/search?AUTHOR=Herodotus&WORK=Historiae&DOCS_PER_PAGE=50">45 papyri</a>.',
         date: -440,
         qty: 54, /* 27 */
-        range: [25, 1350],
+        quantity: {
+            fragments: 45,
+            manuscripts: 9,
+        },
+        earliest: {
+            fragment: 50,
+            manuscript: 900,
+        },
+        range: [50, 1350],
     },
     {
         draft: true,
@@ -45,7 +73,7 @@ window.manuscripts = [
         qty: 10, /* 251 */
         range: [950] /* [800] */
     }
-].filter(m => !m.draft).sort((a, b) => a.date - b.date);
+].filter(m => !m.draft).sort((a, b) => (a.earliest.manuscript - a.date) - (b.earliest.manuscript - b.date));
 
 
 
