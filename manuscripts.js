e6723a9ebbf6827adb94b2460d4becc38e97f04a
Joseph Neill <josephneill2@gmail.com>
2022-10-27 18:41:44 -0400

    update data; tweak styling

--- manuscripts.js
+++ manuscripts.js
@@ -4,12 +4,12 @@
 window.manuscripts = [
     {
         name: 'New Testament',
-        summary: 'The books of the New Testament were written by various authors sometime between approximately 50AD and 100AD. (141 papyri, 322-41=281 uncials, 2926? (2911? or 3011?) minuscules, 2484 lectionaries)',
+        summary: 'The books of the New Testament were written by various authors sometime between approximately 50AD and 100AD.  In total, there are around 136 papyri, 3162 manuscripts, and 2492 lectionaries. This totals over 5700 witnesses to the text of the NT. One of the early papyri (P46) containing much of Apostle Paul’s letters dates to AD 175-225.',
         date: 70, /* 50-100 */
-        qty: 5386, /* 5856 */
+        qty: 5654, /* 5856 */
         quantity: {
-            fragments: 141,
-            manuscripts: 5715,
+            fragments: 136,
+            manuscripts: 5654,
         },
         earliest: {
             fragment: 125,
@@ -19,36 +19,37 @@ window.manuscripts = [
         /* 72, 48, 47, 24 
         2177, 615, 721, 329 */
         /* minuscules biblical content = 2070 */
+        /* (141 papyri, 322-41=281 uncials, 2926? (2911? or 3011?) minuscules, 2484 lectionaries) */
     },
     {
         name: 'Iliad',
-        summary: 'This data is just an approximate.',
+        summary: 'The Iliad was written around the year 800 BC. In 2010 Bird states that there are more that 1900 manuscripts of the Iliad, at least 1500 of which are papyri. This would mean that there are around 400 non-papyri manuscripts – a number that I have seen no other information about. However, later figures place the fragments at 1569, and other manuscripts at 188. This would put the total at around 1757 witnesses. The earliest written fragment dates from the 4th century BC.',
         date: -730, /* -700 */
-        qty: 1900,
+        qty: 1569,
         quantity: {
-            fragments: null,
-            manuscripts: 1900,
+            fragments: 1569,
+            manuscripts: 188,
         },
         earliest: {
-            fragment: null,
-            manuscript: 300,
+            fragment: -350,
+            manuscript: 850,
         },
-        range: [300,1200] /* [-415, ...] */
+        range: [850,1200] /* [-415, ...] */
     },
     {
         name: 'Annals (Tacitus)',
-        summary: 'Still need to do better research on this...',
+        summary: 'The Annals was written around the year AD 100 and tells the history of the Roman Empire from the beginning of the reign of Tiberius in AD 14 to the end of the reign of Nero, in AD 68. Tacitus wrote the Annals in at least 16 books, but books 7-10 and parts of books 5, 6, 11 and 16 are missing. The earliest surviving manuscript dates to around AD 850 and the next one to the mid-eleventh century. The remaining 31 manuscripts descend from these two and date to the fifteenth century.',
         date: 100,
         qty: 2,
         quantity: {
             fragments: 0,
-            manuscripts: 2,
+            manuscripts: 33,
         },
         earliest: {
             fragment: null,
             manuscript: 850,
         },
-        range: [850, 1040]
+        range: [850, 1500]
     },
     {
         name: 'Histories (Herodotus)',
@@ -57,7 +58,7 @@ window.manuscripts = [
         qty: 54, /* 27 */
         quantity: {
             fragments: 45,
-            manuscripts: 9,
+            manuscripts: 60,
         },
         earliest: {
             fragment: 50,
